4c241efbcbf117ba2ae893938a0c0f241dd7a3f1
KalaniM <kalakalanicontact@gmail.com>
2019-12-18 17:05:08 +0100

    add all the content

---
+++ src/comp/otherArticles.jsx
@@ -0,0 +1,23 @@
+import React, { PureComponent } from "react";
+
+class OtherArticles extends PureComponent {
+    render() {
+        return (
+            <>
+                <aside>
+                    <h4>Découverte</h4>
+                    <h5>Le Sunfish</h5>
+                </aside>
+                <aside>
+                    <h4>Explorer</h4>
+                    <h5>La saison des baleines à Malaga</h5>
+                </aside>
+                <aside>
+                    <h4>Enquete</h4>
+                    <h5>La migration des petites crevettes bleues</h5>
+                </aside>
+            </>
+        );
+    }
+}
+export default OtherArticles;
---
+++ src/img/index.js
@@ -0,0 +1,2 @@
+export { default as MolaMola } from "./molaMola.png";
+export { default as Logo } from "./logo.png";
