3ed139698cc2c0349ab40f43e954a2a5617d85fb
CarloBerni <carlo.berni@hetic.net>
2019-12-18 21:23:54 +0100

    menu article right

--- src/comp/otherArticles.jsx
+++ src/comp/otherArticles.jsx
@@ -6,10 +6,12 @@ class OtherArticles extends PureComponent {
             <aside >
                 <h4>Découverte</h4>
                 <h5>Le Sunfish</h5>
+                <hr/>
             </aside>
             <aside>
                 <h4>Explorer</h4>
                 <h5>La saison des baleines à Malaga</h5>
+                <hr/>
             </aside>
             <aside>
                 <h4>Enquete</h4>
