e52dce227bf61b8abb9f5e2eb96b099c1bf6fe93
KalaniM <kalakalanicontact@gmail.com>
2019-12-19 00:24:06 +0100

    correct wordings and make few style

--- src/comp/otherArticles.jsx
+++ src/comp/otherArticles.jsx
@@ -1,22 +1,22 @@
 import React, { PureComponent } from "react";
 
 class OtherArticles extends PureComponent {
-    render() {
-        return ( <div>
-            <aside >
-                <h4>Découverte</h4>
-                <h5>Le Sunfish</h5>
-                <hr/>
-            </aside>
-            <aside>
-                <h4>Explorer</h4>
-                <h5>La saison des baleines à Malaga</h5>
-            </aside>
-            <aside>
-                <h4>Enquete</h4>
-                <h5>La migration des petites crevettes bleues</h5>
-            </aside>
-        </div> );
-    }
+  render() {
+    return ( <div>
+      <aside >
+        <h4>Découverte</h4>
+        <h5>Le Sunfish</h5>
+        <hr/>
+      </aside>
+      <aside>
+        <h4>Explorer</h4>
+        <h5>La saison des baleines à Malaga</h5>
+      </aside>
+      <aside>
+        <h4>Enquête</h4>
+        <h5>La migration des petites crevettes bleues</h5>
+      </aside>
+    </div> );
+  }
 }
 export default OtherArticles;
